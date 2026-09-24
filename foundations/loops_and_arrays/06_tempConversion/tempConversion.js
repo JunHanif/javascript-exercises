1ec45b878ea2749f5d594a72064ac3f24cd06057
Hanif Jundana <142137114+JunHanif@users.noreply.github.com>
2026-09-24 12:09:36 +0700

    temp conver

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier
+}
+
+const convertToCelsius = function (fahrenheit) {
+  let celcius = (fahrenheit - 32) * 5 / 9;
+  return round(celcius, 1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  let fahrenheit = celcius * 9 / 5 + 32
+  return round(fahrenheit, 1)
 };
 
 // Do not edit below this line
