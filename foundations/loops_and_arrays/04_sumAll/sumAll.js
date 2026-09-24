65965e504c4d4ac9e7743e2adfa6af365e4a9b39
Hanif Jundana <142137114+JunHanif@users.noreply.github.com>
2026-09-24 11:32:23 +0700

    refactor sum all

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,23 +1,30 @@
 const sumAll = function (start, end) {
-    let arr = [];
-    let a = start;
-    let b = end;
+    // let arr = [];
+    // let a = start;
+    // let b = end;
 
-    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    if (start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)) {
         return "ERROR"
     }
 
-    if (end > start) {
-        for (let i = a; i <= b; i++) {
-            arr.push(i)
-        }
-        return arr.reduce((total, value) => total + value)
-    } else if (start > end) {
-        for (let i = b; i <= a; i++) {
-            arr.push(i)
-        }
-        return arr.reduce((total, value) => total + value)
+    if (start > end) {
+        [start, end] = [end, start];
     }
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) { sum += i }
+    return sum
+    // if (end > start) {
+    //     for (let i = a; i <= b; i++) {
+    //         arr.push(i)
+    //     }
+    //     return arr.reduce((total, value) => total + value)
+    // } else if (start > end) {
+    //     for (let i = b; i <= a; i++) {
+    //         arr.push(i)
+    //     }
+    //     return arr.reduce((total, value) => total + value)
+    // }
 };
 
 // Do not edit below this line
