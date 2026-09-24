5f6eeb01b1a7ad613e9466a897eca5cc21bac329
Hanif Jundana <142137114+JunHanif@users.noreply.github.com>
2026-09-24 11:25:44 +0700

    sum all

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let arr = [];
+    let a = start;
+    let b = end;
 
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
+
+    if (end > start) {
+        for (let i = a; i <= b; i++) {
+            arr.push(i)
+        }
+        return arr.reduce((total, value) => total + value)
+    } else if (start > end) {
+        for (let i = b; i <= a; i++) {
+            arr.push(i)
+        }
+        return arr.reduce((total, value) => total + value)
+    }
 };
 
 // Do not edit below this line
